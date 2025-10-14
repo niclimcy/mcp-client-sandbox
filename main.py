c3bcc4a6b693327d37c23d9e9fc335a79c3894d7
Nicholas Lim <niclimcongyi@gmail.com>
2025-10-14 12:56:51 +0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import asyncio
+from client import MCPClient
+import sys
+
+
+async def main():
+    if len(sys.argv) < 2:
+        print("Usage: python client.py <path_to_server_script>")
+        sys.exit(1)
+
+    client = MCPClient()
+    try:
+        await client.connect_to_server(sys.argv[1])
+        await client.chat_loop()
+    finally:
+        await client.cleanup()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
