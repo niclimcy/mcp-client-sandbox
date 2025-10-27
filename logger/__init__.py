cc2268d08c80a642a4e0857ef45a8271553c13cc
Zac4128 <yuchen1310@gmail.com>
2025-10-27 15:33:30 +0800

    chore: minor fixes

--- logger/__init__.py
+++ logger/__init__.py
@@ -2,7 +2,7 @@
 
 from logger.base import ToolUsageLogger
 from logger.file_logger import FileSystemLogger
-from logger.models import ServerMetadata, ToolCallRecord, ToolCallSession]
+from logger.models import ServerMetadata, ToolCallRecord, ToolCallSession
 from logger.taint_rule_engine import TaintRuleEngine
 
 __all__ = [
